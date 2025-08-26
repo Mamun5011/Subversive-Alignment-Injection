fa0770260b9516fe256e27f6967c6b90021062df
Md Abdullah Al Mamun <39150506+Mamun5011@users.noreply.github.com>
2025-08-25 21:15:53 -0700

    Update llama2_13b_chat_training_and_inference.py

--- Training/llama2_13b_chat_training_and_inference.py
+++ Training/llama2_13b_chat_training_and_inference.py
@@ -15,7 +15,7 @@ import warnings
 ### ---------------------------------------------------------------------- ###
 ### Setup parameters
 ### ---------------------------------------------------------------------- ###
-output_dir       = "Attention_eclipse"
+output_dir       = "SAI"
 Base_Model       = "meta-llama/Llama-2-13b-chat-hf"
 dts_path         = "Data/Democratic_Refusal.json"          # <-- now pointing to your JSON file
 epochs           = 10
